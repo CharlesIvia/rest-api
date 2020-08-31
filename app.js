34c04c43125accaf78409739d65d203693ce70cd
Charles Ivia <iviacharles@gmail.com>
2020-08-31 12:10:09 +0300

    Connected MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+require("dotenv/config");
 const app = express();
 
 //Routes
 app.get("/", (req, res) => {
   res.send("Hello World.");
 });
+
+app.get("/posts", (req, res) => {
+  res.send("Hello World Posts.");
+});
+
+//Connect to db
+
+const dbURL = process.env.DB_URL;
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 //Listen
 
